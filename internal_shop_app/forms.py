65d6771389c39b815339063ef867091a8456a7e6
henryglez02 <henryglez02@gmail.com>
2024-04-06 14:48:29 -0400

    fix: clean unable code

--- internal_shop_app/forms.py
+++ internal_shop_app/forms.py
@@ -3,12 +3,6 @@ from django import forms
 from internal_shop_app.models import Producto, Venta
 
 
-# class CrearProducto(forms.Form):
-#     nombre = forms.CharField(label="Nombre")
-#     precio = forms.FloatField(label="Precio")
-#     categoria = forms.CharField(label="Categoría")
-#     reserva = forms.IntegerField(label="Reserva")
-
 class VentaForm(forms.ModelForm):
     class Meta:
         model = Venta
