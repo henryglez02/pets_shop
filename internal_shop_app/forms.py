8e3d13087fe25dd63d63a5ec16c252994db0d9c5
henryglez02 <henryglez02@gmail.com>
2024-04-10 09:48:17 -0400

    add:form_style

--- internal_shop_app/forms.py
+++ internal_shop_app/forms.py
@@ -7,9 +7,17 @@ class VentaForm(forms.ModelForm):
     class Meta:
         model = Venta
         fields = ['id_producto', 'cantidad']
+        widgets = {'cantidad': forms.NumberInput(
+            attrs={'class': 'border border-1 rounded-md px-1'})}
 
 
-class CrearProducto(forms.ModelForm):
+class ProductoForm(forms.ModelForm):
     class Meta:
         model = Producto
         fields = ['nombre', 'precio', 'categoria', 'reserva']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'border border-1 rounded-md px-1'}),
+            'precio': forms.NumberInput(attrs={'class': 'border border-1 rounded-md px-1'}),
+            'categoria': forms.TextInput(attrs={'class': 'border border-1 rounded-md px-1'}),
+            'reserva': forms.NumberInput(attrs={'class': 'border border-1 rounded-md px-1'}),
+        }
